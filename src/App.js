44e6aab03c760d484923d8fffdf513829259fd08
degroote <lucaxx@gmail.com>
2016-12-26 20:08:37 -0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
-import Logo from './logo';
-import './App.css';
 import { ThemeProvider } from 'styled-components'
 
 const theme = {}
@@ -10,15 +8,7 @@ class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <div className="App">
-          <div className="App-header">
-            <Logo width="80" height="80"/>
-            <h2>Welcome to Inferno</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        foo
       </ThemeProvider>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import Inferno from 'inferno';
-import App from './App';
-import './index.css';
+import Component from 'inferno-component';
+import firebase from 'firebase'
+
+class App extends Component {
+  
+  componentDidMount() {
+    const facebookPermissions = [
+      'email',
+      'public_profile',
+    ];
+    firebase.initializeApp({
+      //private
+    });
+    const provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope(facebookPermissions.join(','))
+    firebase.auth().signInWithRedirect(provider)
+          .then(console.log).catch(console.error)
+  }
+
+  render() {
+    return (
+      <div>
+        foo
+      </div>
+    );
+  }
+}
 
 Inferno.render(
   <App />,
